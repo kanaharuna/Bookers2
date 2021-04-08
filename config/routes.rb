32d791156fa675537c456d557b462ea966891a16
kanaharuna <paris.makaron.81@gmail.com>
2021-04-08 09:56:33 +0000

    [Add]投稿・編集・削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'home/about' => "homes#about"
   devise_for :users
+  resources :users,only:[:show,:index,:edit,:update]
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_042549) do
+ActiveRecord::Schema.define(version: 2021_04_08_020347) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_04_07_042549) do
     t.datetime "remember_created_at"
     t.string "name"
     t.text "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
